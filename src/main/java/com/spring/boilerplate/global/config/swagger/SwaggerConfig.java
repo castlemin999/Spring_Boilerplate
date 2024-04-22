0b76217194dae92f5fabbbacfc80806f001ce385
castlemin999 <castlemin999@gmail.com>
2024-04-22 22:58:25 +0900

    feat: Swagger Response 예시 설정

--- src/main/java/com/spring/boilerplate/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/spring/boilerplate/global/config/swagger/SwaggerConfig.java
@@ -3,9 +3,13 @@ package com.spring.boilerplate.global.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -75,5 +79,24 @@ public class SwaggerConfig {
                 .title("Spring Boilerplate")
                 .description("스프링 보일러 플레이트 스웨거 설정 입니다.")
                 .version("1.0.0");
+    }
+
+    // 공통으로 사용되는 response 설정
+    @Bean
+    public GlobalOpenApiCustomizer customerGlobalHeaderOpenApiCustomizer() {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+                ApiResponses apiResponses = operation.getResponses();
+                apiResponses.addApiResponse("200", createApiResponse(apiResponses.get("200").getDescription(), apiResponses.get("200").getContent()));
+                apiResponses.addApiResponse("400", createApiResponse("Bad Request", null));
+                apiResponses.addApiResponse("401", createApiResponse("Token Error", null));
+                apiResponses.addApiResponse("404", createApiResponse("Not Found", null));
+                apiResponses.addApiResponse("500", createApiResponse("Server Error", null));
+            }));
+        };
+    }
+
+    public ApiResponse createApiResponse(String message, Content content){
+        return new ApiResponse().description(message).content(content);
     }
 }
